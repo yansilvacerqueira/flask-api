87b40a490844e6ea6324ef06b5a805be23ca6421
Yan Cerqueira <yansilvacerqueira@outlook.com>
2024-10-19 21:46:32 -0300

    feat: adding Dockerfile and updating the app file according with flake8

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {'message': 'user 1'}
 
+
 class User(Resource):
     def post(self):
         return {'message': 'user'}
-    
+
     def get(self, cpf):
         return {'cpf': cpf}
-    
+
+
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
